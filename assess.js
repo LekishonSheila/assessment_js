00d39f103c497f50fe24db628931f8fd0d686698
lekishonsheila <lekishonsheila@gmail.com>
2023-05-01 09:02:30 +0300

    first

---
+++ assess.js
@@ -0,0 +1,86 @@
+// Create a function called getAvailableBooks that returns an array of all available
+// books.
+function getAvailableBooks(books){
+    if (books) {
+        books.listOf();
+        
+    }
+    return(getAvailableBooks)
+     
+    }
+
+
+const books = [
+{ title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+isAvailable: true },
+{ title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+isAvailable: false },
+{ title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+1866, isAvailable: true },
+{ title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+isAvailable: false },
+{ title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+];
+console.log(getAvailableBooks)
+
+// Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+function getBookByAuthor(authorsname){
+    if (authorsname) {
+        for (let i = 0; i < books.length; i++) {
+            const element = array[i];    
+        }
+          
+    }
+    return(getBookByAuthor)
+}
+const book = [
+{ title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+isAvailable: true },
+{ title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+isAvailable: false },
+{ title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+1866, isAvailable: true },
+{ title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+isAvailable: false },
+{ title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+];
+
+// console.log(getBooksByAuthor)
+
+// Create a function addNewBook that takes a book object as an argument and adds it
+// to the library, ensuring that the new book has all required properties (title, author,
+// publicationYear, and isAvailable).
+function addNewBook(book) {
+    const book = ({});
+    const Newbook = Newbook({title:'Born a crime'},{author:'Trevor Noah'},{publicationYear:2000},{isAvailable:true});
+    Newbook.add(books)
+         
+}
+const bookss = [
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+    isAvailable: true },
+    { title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+    isAvailable: false },
+    { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+    1866, isAvailable: true },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+    isAvailable: false },
+    { title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+    ];
+console.log(addNewBook);
+
+// Create a function checkoutBook that takes a book title as an argument and changes
+// the book's isAvailable property to false. If the book is not found in the library, the
+// function should return a message indicating that the book is not available.
+function checkoutBook(booktitle) {
+    // if () {
+    //     return ("book not available")
+    // }
+    
+    
+}
+// console.log(checkoutBook);
+
+
+
